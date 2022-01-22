a907e1576a737fef346057509d89db774d4db203
Luis Michaelis <lmichaelis.all+dev@gmail.com>
2022-01-22 15:00:21 +0100

    PlayerEventListener: move handler for interacting with Aurora itself into a separate class

---
+++ src/main/java/de/lmichaelis/aurora/Interactions.java
@@ -0,0 +1,87 @@
+// Copyright (c) 2022. Luis Michaelis
+// SPDX-License-Identifier: LGPL-3.0-only
+package de.lmichaelis.aurora;
+
+import de.lmichaelis.aurora.model.Claim;
+import de.lmichaelis.aurora.model.User;
+import de.lmichaelis.aurora.task.ClaimVisualizationTask;
+import org.bukkit.*;
+import org.bukkit.entity.Player;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.jetbrains.annotations.NotNull;
+
+public final class Interactions {
+	public static void onAuroraInteract(final Aurora plugin, final @NotNull Player player, final @NotNull Material tool) {
+		final var rayTraceResult = player.rayTraceBlocks(100, FluidCollisionMode.SOURCE_ONLY);
+
+		if (rayTraceResult == null) {
+			player.sendMessage(plugin.config.messages.tooFarAway);
+			return;
+		}
+
+		final var targetedLocation = rayTraceResult.getHitPosition().toLocation(player.getWorld());
+		final var targetedClaim = Claim.getClaim(targetedLocation);
+
+		if (tool == plugin.config.claimCreationTool) {
+			// Feature: Create a claim.
+			if (targetedClaim != null) {
+				player.sendMessage(plugin.config.messages.alreadyClaimed);
+				return;
+			}
+
+			final var previousLocationMeta = player.getMetadata("aurora.claimBlockSelection");
+			final var user = User.fromMetadata(player);
+
+			if (previousLocationMeta.size() == 0) {
+				player.sendMessage(plugin.config.messages.claimCornerSet);
+				player.setMetadata("aurora.claimBlockSelection", new FixedMetadataValue(plugin, targetedLocation));
+				return;
+			}
+
+			final var previousLocation = (Location) previousLocationMeta.get(0).value();
+			final var remainingClaimBlocks = user.totalClaimBlocks - user.usedClaimBlocks;
+
+			final var sizeX = Math.abs(previousLocation.getBlockX() - targetedLocation.getBlockX()) + 1;
+			final var sizeZ = Math.abs(previousLocation.getBlockZ() - targetedLocation.getBlockZ()) + 1;
+
+			if (sizeX * sizeZ > remainingClaimBlocks) {
+				player.sendMessage(plugin.config.messages.needMoreClaimBlocks.formatted(sizeX * sizeZ - remainingClaimBlocks));
+				return;
+			}
+
+			if (Claim.intersects(previousLocation, targetedLocation, true)) {
+				player.sendMessage(plugin.config.messages.wouldOverlapAnotherClaim);
+				return;
+			}
+
+			player.sendMessage(plugin.config.messages.claimCreated.formatted(sizeX, sizeZ));
+			player.removeMetadata("aurora.claimBlockSelection", plugin);
+
+			user.usedClaimBlocks += sizeX * sizeZ;
+			user.update();
+
+			new Claim(player.getUniqueId(), "test", previousLocation, targetedLocation).save();
+		} else {
+			// Feature: Inspect a claim.
+			if (targetedClaim == null) {
+				player.sendMessage(plugin.config.messages.notAClaim);
+				return;
+			}
+
+			player.sendMessage(plugin.config.messages.blockClaimedBy.formatted(
+					Bukkit.getOfflinePlayer(targetedClaim.owner).getName())
+			);
+
+			// TODO: prevent multiple tasks per claim per player
+			final var visTask = Bukkit.getScheduler().runTaskTimer(
+					plugin,
+					new ClaimVisualizationTask(targetedClaim, player, Color.RED),
+					0, 5
+			);
+
+			Bukkit.getScheduler().runTaskLater(plugin, () -> {
+				Bukkit.getScheduler().cancelTask(visTask.getTaskId());
+			}, 20 * 20);
+		}
+	}
+}
--- src/main/java/de/lmichaelis/aurora/listener/PlayerEventListener.java
+++ src/main/java/de/lmichaelis/aurora/listener/PlayerEventListener.java
@@ -3,14 +3,12 @@
 package de.lmichaelis.aurora.listener;
 
 import de.lmichaelis.aurora.Aurora;
+import de.lmichaelis.aurora.Interactions;
 import de.lmichaelis.aurora.Predicates;
 import de.lmichaelis.aurora.model.Claim;
 import de.lmichaelis.aurora.model.Group;
 import de.lmichaelis.aurora.model.User;
-import de.lmichaelis.aurora.task.ClaimVisualizationTask;
-import org.bukkit.*;
 import org.bukkit.block.Container;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -24,80 +22,6 @@ import org.jetbrains.annotations.NotNull;
 public final class PlayerEventListener extends BaseListener {
 	public PlayerEventListener(final Aurora plugin) {
 		super(plugin);
-	}
-
-	private void onAuroraInteract(final @NotNull Player player, final @NotNull Material tool) {
-		final var rayTraceResult = player.rayTraceBlocks(100, FluidCollisionMode.SOURCE_ONLY);
-
-		if (rayTraceResult == null) {
-			player.sendMessage(plugin.config.messages.tooFarAway);
-			return;
-		}
-
-		final var targetedLocation = rayTraceResult.getHitPosition().toLocation(player.getWorld());
-		final var targetedClaim = Claim.getClaim(targetedLocation);
-
-		if (tool == plugin.config.claimCreationTool) {
-			// Feature: Create a claim.
-			if (targetedClaim != null) {
-				player.sendMessage(plugin.config.messages.alreadyClaimed);
-				return;
-			}
-
-			final var previousLocationMeta = player.getMetadata("aurora.claimBlockSelection");
-			final var user = User.fromMetadata(player);
-
-			if (previousLocationMeta.size() == 0) {
-				player.sendMessage(plugin.config.messages.claimCornerSet);
-				player.setMetadata("aurora.claimBlockSelection", new FixedMetadataValue(plugin, targetedLocation));
-				return;
-			}
-
-			final var previousLocation = (Location) previousLocationMeta.get(0).value();
-			final var remainingClaimBlocks = user.totalClaimBlocks - user.usedClaimBlocks;
-
-			final var sizeX = Math.abs(previousLocation.getBlockX() - targetedLocation.getBlockX()) + 1;
-			final var sizeZ = Math.abs(previousLocation.getBlockZ() - targetedLocation.getBlockZ()) + 1;
-
-			if (sizeX * sizeZ > remainingClaimBlocks) {
-				player.sendMessage(plugin.config.messages.needMoreClaimBlocks.formatted(sizeX * sizeZ - remainingClaimBlocks));
-				return;
-			}
-
-			if (Claim.intersects(previousLocation, targetedLocation, true)) {
-				player.sendMessage(plugin.config.messages.wouldOverlapAnotherClaim);
-				return;
-			}
-
-			player.sendMessage(plugin.config.messages.claimCreated.formatted(sizeX, sizeZ));
-			player.removeMetadata("aurora.claimBlockSelection", plugin);
-
-			user.usedClaimBlocks += sizeX * sizeZ;
-			user.update();
-
-			new Claim(player.getUniqueId(), "test", previousLocation, targetedLocation).save();
-		} else {
-			// Feature: Inspect a claim.
-			if (targetedClaim == null) {
-				player.sendMessage(plugin.config.messages.notAClaim);
-				return;
-			}
-
-			player.sendMessage(plugin.config.messages.blockClaimedBy.formatted(
-					Bukkit.getOfflinePlayer(targetedClaim.owner).getName())
-			);
-
-			// TODO: prevent multiple tasks per claim per player
-			final var visTask = Bukkit.getScheduler().runTaskTimer(
-					plugin,
-					new ClaimVisualizationTask(targetedClaim, player, Color.RED),
-					0, 5
-			);
-
-			Bukkit.getScheduler().runTaskLater(plugin, () -> {
-				Bukkit.getScheduler().cancelTask(visTask.getTaskId());
-			}, 20 * 20);
-		}
 	}
 
 	/**
@@ -141,7 +65,7 @@ public final class PlayerEventListener extends BaseListener {
 		// Rule: If the player is holding the claim creation or investigation tool, perform
 		//       the required action and cancel the event
 		if (holdType == plugin.config.claimCreationTool || holdType == plugin.config.claimInvestigationTool) {
-			onAuroraInteract(player, holdType);
+			Interactions.onAuroraInteract(plugin, player, holdType);
 			event.setCancelled(true);
 			return;
 		}
