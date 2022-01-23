359b12ecda5a06b6bec4ac5b8bbd165fe72164e0
Luis Michaelis <lmichaelis.all+dev@gmail.com>
2022-01-23 19:43:20 +0100

    RaidEventListener: implement handler for `RaidTriggerEvent`

--- src/main/java/de/lmichaelis/aurora/listener/RaidEventListener.java
+++ src/main/java/de/lmichaelis/aurora/listener/RaidEventListener.java
@@ -3,6 +3,11 @@
 package de.lmichaelis.aurora.listener;
 
 import de.lmichaelis.aurora.Aurora;
+import de.lmichaelis.aurora.model.Claim;
+import de.lmichaelis.aurora.model.Group;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.raid.RaidTriggerEvent;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Event handlers for raid events.
@@ -10,5 +15,20 @@ import de.lmichaelis.aurora.Aurora;
 public final class RaidEventListener extends BaseListener {
 	public RaidEventListener(final Aurora plugin) {
 		super(plugin);
+	}
+
+	@EventHandler(ignoreCancelled = true)
+	public void onRaidTrigger(final @NotNull RaidTriggerEvent event) {
+		final var player = event.getPlayer();
+		final var claim = Claim.getClaim(player.getLocation());
+
+		// Rule: Players can always trigger raid outside of claims
+		if (claim == null) return;
+
+		// Rule: Triggering raids inside claims requires the player to be in the BUILD group
+		if (claim.isAllowed(player, Group.BUILD)) return;
+
+		player.sendMessage(plugin.config.messages.raidPrevented);
+		event.setCancelled(true);
 	}
 }
