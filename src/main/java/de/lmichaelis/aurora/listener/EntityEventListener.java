84e00271d5689b9697b5896cdc826528756da7f8
Luis Michaelis <lmichaelis.all+dev@gmail.com>
2022-01-24 07:55:24 +0100

    EntityEventListener: implement handler for `EntityBreakDoorEvent`

--- src/main/java/de/lmichaelis/aurora/listener/EntityEventListener.java
+++ src/main/java/de/lmichaelis/aurora/listener/EntityEventListener.java
@@ -3,6 +3,10 @@
 package de.lmichaelis.aurora.listener;
 
 import de.lmichaelis.aurora.Aurora;
+import de.lmichaelis.aurora.model.Claim;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityBreakDoorEvent;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Event handlers for entity events.
@@ -10,5 +14,18 @@ import de.lmichaelis.aurora.Aurora;
 public final class EntityEventListener extends BaseListener {
 	public EntityEventListener(final Aurora plugin) {
 		super(plugin);
+	}
+
+	@EventHandler(ignoreCancelled = true)
+	public void onEntityBreakDoor(final @NotNull EntityBreakDoorEvent event) {
+		final var claim = Claim.getClaim(event.getBlock().getLocation());
+
+		// Rule: Zombies can always break doors outside of claims
+		if (claim == null) return;
+
+		// Rule: If mob griefing is enabled in the claim, zombies can break doors
+		if (claim.mobGriefing) return;
+
+		event.setCancelled(true);
 	}
 }
