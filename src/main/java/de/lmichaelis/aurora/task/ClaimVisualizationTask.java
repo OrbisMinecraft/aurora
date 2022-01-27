a445e870aaaa92d460c08f794a72950976b3b7a9
Luis Michaelis <lmichaelis.all+dev@gmail.com>
2022-01-27 13:32:24 +0100

    ClaimVisualizationTask: display particles with proper spacing and don't display particles thar are too far away

--- src/main/java/de/lmichaelis/aurora/task/ClaimVisualizationTask.java
+++ src/main/java/de/lmichaelis/aurora/task/ClaimVisualizationTask.java
@@ -6,6 +6,7 @@ import de.lmichaelis.aurora.Aurora;
 import de.lmichaelis.aurora.model.Claim;
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
+import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -14,74 +15,66 @@ public class ClaimVisualizationTask implements Runnable {
 	private static final Particle PARTICLE = Particle.REDSTONE.builder().force(false).particle();
 	private final @NotNull Player player;
 
-	private final Claim claim;
-	private final double particleDensityX, particleDensityY, particleDensityZ;
 	private final Particle.DustOptions options;
+	private final Claim claim;
 	private int id;
-
 
 	public ClaimVisualizationTask(final @NotNull Claim claim, final @NotNull Player player, Color color) {
 		this.player = player;
 		this.claim = claim;
 		this.options = new Particle.DustOptions(color, 3);
-
-		this.particleDensityX = (claim.maxX - claim.minX) / 2.d;
-		this.particleDensityY = (claim.maxY - claim.minY) / 2.d;
-		this.particleDensityZ = (claim.maxZ - claim.minZ) / 2.d;
 	}
 
 	public static @NotNull ClaimVisualizationTask spawn(final @NotNull Claim claim, final @NotNull Player player, Color color, final int cancelAfterTicks) {
 		final var task = new ClaimVisualizationTask(claim, player, color);
-		task.id = Bukkit.getScheduler().scheduleSyncRepeatingTask(Aurora.instance, task, 0, 5);
+		task.id = Bukkit.getScheduler().scheduleSyncRepeatingTask(Aurora.instance, task, 0, 20);
 		Bukkit.getScheduler().scheduleSyncDelayedTask(Aurora.instance, task::cancel, cancelAfterTicks);
 		return task;
 	}
 
-	private void drawLineX(double x, double y, double z) {
-		player.spawnParticle(PARTICLE, x, y, z, (int) Math.ceil(particleDensityX / 2), particleDensityX / 2.d, 0, 0, options);
-	}
 
-	private void drawLineY(double x, double y, double z) {
-		player.spawnParticle(PARTICLE, x, y, z, (int) Math.max(particleDensityY / 3, 5), 0, particleDensityY / 2.d, 0, options);
+	private void drawLine(double x1, double y1, double z1, double x2, double y2, double z2) {
+		double distanceX = Math.abs(x2 - x1), distanceY = Math.abs(y2 - y1), distanceZ = Math.abs(z2 - z1);
+		double stepCount = (Math.max(Math.max(distanceX, distanceY), distanceZ)) / 2;
+		double stepSizeX = distanceX / stepCount, stepSizeY = distanceY / stepCount, stepSizeZ = distanceZ / stepCount;
 
-		// Don't show highlights for sub-claims.
-		if (claim.parent == null) {
-			player.spawnParticle(PARTICLE, x, this.player.getLocation().getY(), z, 20, 0, 10, 0, options);
+		final var location = new Location(player.getWorld(), x1, y1, z1);
+
+		for (int step = 0; step < stepCount + 1; step++) {
+			if (location.distanceSquared(player.getLocation()) > 100 * 100) break;
+			player.spawnParticle(PARTICLE, location, 1, 0, 0, 0, options);
+			location.add(stepSizeX, stepSizeY, stepSizeZ);
 		}
 	}
 
-	private void drawLineZ(double x, double y, double z) {
-		player.spawnParticle(PARTICLE, x, y, z, (int) Math.ceil(particleDensityZ / 2), 0, 0, particleDensityZ / 2.d, options);
+	private void drawVerticalLine(double x, double z, double y1, double y2) {
+		drawLine(x, y1, z, x, y2, z);
+	}
+
+	private void drawHorizontalLineX(double x, double y, double z1, double z2) {
+		drawLine(x, y, z1, x, y, z2);
+	}
+
+	private void drawHorizontalLineZ(double y, double z, double x1, double x2) {
+		drawLine(x1, y, z, x2, y, z);
 	}
 
 	@Override
 	public void run() {
-		// Draw the corners
-		drawLineY(claim.minX, claim.minY + particleDensityY + 0.5, claim.minZ);
-		drawLineY(claim.minX, claim.minY + particleDensityY + 0.5, claim.maxZ + 1);
-		drawLineY(claim.maxX + 1, claim.minY + particleDensityY + 0.5, claim.minZ);
-		drawLineY(claim.maxX + 1, claim.minY + particleDensityY + 0.5, claim.maxZ + 1);
+		drawVerticalLine(claim.minX, claim.minZ, claim.minY, claim.maxY);
+		drawVerticalLine(claim.maxX + 1, claim.minZ, claim.minY, claim.maxY);
+		drawVerticalLine(claim.minX, claim.maxZ + 1, claim.minY, claim.maxY);
+		drawVerticalLine(claim.maxX + 1, claim.maxZ + 1, claim.minY, claim.maxY);
 
-		// Draw the bottom rectangle
-		drawLineX(claim.minX + particleDensityX, claim.minY, claim.minZ);
-		drawLineX(claim.minX + particleDensityX, claim.minY, claim.maxZ + 1);
-		drawLineZ(claim.minX, claim.minY, claim.minZ + particleDensityZ);
-		drawLineZ(claim.maxX + 1, claim.minY, claim.minZ + particleDensityZ);
+		drawHorizontalLineX(claim.minX, claim.minY, claim.minZ, claim.maxZ + 1);
+		drawHorizontalLineX(claim.maxX + 1, claim.minY, claim.minZ, claim.maxZ + 1);
+		drawHorizontalLineX(claim.minX, claim.maxY + 1, claim.minZ, claim.maxZ + 1);
+		drawHorizontalLineX(claim.maxX + 1, claim.maxY + 1, claim.minZ, claim.maxZ + 1);
 
-		// Draw the top rectangle
-		drawLineX(claim.minX + particleDensityX, claim.maxY + 1, claim.minZ);
-		drawLineX(claim.minX + particleDensityX, claim.maxY + 1, claim.maxZ + 1);
-		drawLineZ(claim.minX, claim.maxY + 1, claim.minZ + particleDensityZ);
-		drawLineZ(claim.maxX + 1, claim.maxY + 1, claim.minZ + particleDensityZ);
-
-		// Draw a line at the player's head position only if we're not inspecting a subclaim
-		if (claim.parent == null) {
-			final var playerY = (int) player.getLocation().getY() + 1;
-			drawLineX(claim.minX + particleDensityX, playerY, claim.minZ);
-			drawLineX(claim.minX + particleDensityX, playerY, claim.maxZ + 1);
-			drawLineZ(claim.minX, playerY, claim.minZ + particleDensityZ);
-			drawLineZ(claim.maxX + 1, playerY, claim.minZ + particleDensityZ);
-		}
+		drawHorizontalLineZ(claim.minY, claim.minZ, claim.minX, claim.maxX);
+		drawHorizontalLineZ(claim.minY, claim.maxZ + 1, claim.minX, claim.maxX);
+		drawHorizontalLineZ(claim.maxY + 1, claim.minZ, claim.minX, claim.maxX);
+		drawHorizontalLineZ(claim.maxY + 1, claim.maxZ + 1, claim.minX, claim.maxX);
 	}
 
 	public void cancel() {
