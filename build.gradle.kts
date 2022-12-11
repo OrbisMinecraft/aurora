e5a09a6fe5c2ac809cc982599efcbff15927d763
Luis Michaelis <me@lmichaelis.de>
2022-12-11 13:18:06 +0100

    Build: Switch to the Kotlin DSL for Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,74 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar;
+
+plugins {
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("java")
+}
+
+val gitCommitHash = ProcessBuilder("git", "rev-parse", "--verify", "--short", "HEAD")
+    .redirectOutput(ProcessBuilder.Redirect.PIPE)
+    .start()
+    .inputStream
+    .bufferedReader()
+    .readLine()
+    .trim()
+
+val auroraVersion = findProperty("auroraVersion") as String?
+val auroraJdbcImplementation = findProperty("auroraJdbcImplementation") as String?
+
+
+group = "de.lmichaelis"
+version = "$auroraVersion+git.$gitCommitHash"
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
+    implementation("com.j256.ormlite:ormlite-jdbc:5.7")
+    implementation("org.jetbrains:annotations:23.0.0")
+
+    // optionally bundle the JDBC driver
+    if (auroraJdbcImplementation != null) {
+        implementation(auroraJdbcImplementation)
+    }
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("com.github.seeseemelk:MockBukkit-v1.18:1.15.5")
+}
+
+val targetJavaVersion = 17
+java {
+    val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    if (JavaVersion.current() < javaVersion) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+    }
+}
+
+tasks.withType<JavaCompile> {
+    if (targetJavaVersion >= 10 || JavaVersion.current().isJava10Compatible()) {
+        options.release.set(targetJavaVersion)
+    }
+}
+
+tasks.withType<ShadowJar> {
+    archiveFileName.set("aurora-${archiveVersion.get()}.jar")
+}
+
+tasks.withType<ProcessResources> {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
